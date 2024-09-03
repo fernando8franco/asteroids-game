d9dd853606ba61c4e066090a99923f94259666f6
fernando8franco <francoduranluisfernando@gmail.com>
2024-09-03 16:17:03 -0600

    moving around methods added

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
-    
+
     while True:
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
+        player.update(dt)
         screen.fill("black")
         player.draw(screen)
 
